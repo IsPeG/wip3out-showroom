31b18529332c07121d9ef4a73b81b8c730bb39b5
IsPeG <igr13267@gmail.com>
2024-01-07 19:14:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css";
+import Hero from "./components/Hero.jsx"
+
+function App() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <section className="hero-section">
+      <h1 className="hero-section-title">Wipeout three<span id="hero-section-title-cursor">_</span></h1>
+      <h2 className="hero-section-subtitle">showroom</h2>
+
+      <div className="hero-section-hbars-container">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+    </section>
+  );
+}
